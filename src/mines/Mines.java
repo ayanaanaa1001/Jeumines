db12c24d92bbc66484f29d194520eaa9906edcb8
ayanaanaa <ayaayouch100.com>
2024-11-12 13:29:49 +0100

    small change

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -1,36 +1,34 @@
 package mines;
 
 import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 // Source: http://zetcode.com/tutorials/javagamestutorial/minesweeper/
 
 public class Mines extends JFrame {
-	private static final long serialVersionUID = 4772165125287256837L;
-	
-	private final int WIDTH = 250;
+    private static final long serialVersionUID = 4772165125287256837L;
+
+    private final int WIDTH = 250;
     private final int HEIGHT = 290;
 
-    private JLabel statusbar;
-    
     public Mines() {
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setTitle("Minesweeper");
 
-        statusbar = new JLabel("");
+        // Declare statusbar as a local variable inside the constructor
+        JLabel statusbar = new JLabel("");
         add(statusbar, BorderLayout.SOUTH);
 
+        // Pass the statusbar to the Board constructor
         add(new Board(statusbar));
 
         setResizable(false);
         setVisible(true);
     }
-    
+
     public static void main(String[] args) {
         new Mines();
     }
